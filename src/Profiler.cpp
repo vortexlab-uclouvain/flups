99c1493bb7a638bc979c7b05a84917c805ad68c5
tgillis <thomas.gillis@uclouvain.be>
2019-09-11 22:27:31 +0200

    :lipstick: improved profiler IO

--- src/Profiler.cpp
+++ src/Profiler.cpp
@@ -225,7 +225,7 @@ void TimerAgent::disp(FILE* file,const int level, const double totalTime){
 
         // printf the important information
         if (rank == 0) {
-            printf("%-25.25s|  \t%07.4f\t\t%07.4f\t\t%07.4f\t\t%.6f\t%.6f\t%.6f\t%.6f\t%09.0f\n", myname.c_str(), glob_percent, loc_percent, meanTime, selfTime, meanTimePerCount, minTime, maxTime, meanCount);
+            printf("%-25.25s|  \t%07.4f\t\t%07.4f\t\t%9.6f\t%9.6f\t%9.6f\t%9.6f\t%9.6f\t%09.0f\n", myname.c_str(), glob_percent, loc_percent, meanTime, selfTime, meanTimePerCount, minTime, maxTime, meanCount);
             fprintf(file, "%s;%09.6f;%09.6f;%09.6f;%09.6f;%09.6f;%09.6f;%09.6f;%09.0f\n", _name.c_str(), glob_percent, loc_percent, meanTime, selfTime, meanTimePerCount, minTime, maxTime, meanCount);
         }
     }
@@ -371,7 +371,8 @@ void Profiler::disp() {
     if (rank == 0) {
         printf("===================================================================================================================================================\n");
         printf("        PROFILER %s  \n", _name.c_str());
-        printf("\t-NAME-   \t\t\t-%% global-\t-%% local-\t-Total time-\t-Self time-\t-time/call-\t-Min tot time-\t-Max tot time-\t-Mean cnt-\n");
+        // printf("\t-NAME-   \t\t\t-%% global-\t-%% local-\t-Total time-\t-Self time-\t-time/call-\t-Min tot time-\t-Max tot time-\t-Mean cnt-\n");
+        printf("%18.18s\t |%15.15s%15.15s     %15.15s%15.15s %15.15s    %15.15s %15.15s%15.15s\n","-NAME-", "-% global-", "-% local-", "-Total time-", "-Self time-", "-time/call-", "-Min tot time-", "-Max tot time-","-Mean cnt-");
     }
     // get the global timing
     double localTotalTime = _timeMap["root"]->timeAcc();
