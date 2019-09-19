70487f2c25db82891dc6463c6c08bc2edab7e4c0
DeeGee <denis-gabriel.caprace@uclouvain.be>
2019-09-19 16:30:53 +0200

    :tada: cleaning pipeline now that the results of the tests are ok since the merge of dev_openmp

--- scripts/check_res.py
+++ scripts/check_res.py
@@ -12,13 +12,19 @@ def check_res(i, file):
     n_mistake = 0
 
     fcurr = open('./data/'+file,'r')
-    fref  = open('./data_ref/'+file,'r')
-
+    
     #creating a dictionnary with reference data
-    dicref = {}
-    for line in csv.reader(fref,delimiter=' '):
-        buff = list(line)  
-        dicref.update({buff[0] : [float(buff[1]),float(buff[2])] })
+    try: 
+        fref  = open('./data_ref/'+file,'r')
+
+        dicref = {}
+        for line in csv.reader(fref,delimiter=' '):
+            buff = list(line)  
+            dicref.update({buff[0] : [float(buff[1]),float(buff[2])] })
+
+        fref.close()
+    except FileNotFoundError:
+        dicref = {}
 
     #comparing current results with reference
     for line in csv.reader(fcurr,delimiter=' '):
@@ -44,6 +50,5 @@ def check_res(i, file):
             n_mistake +=1
 
     fcurr.close()
-    fref.close()
 
     return n_mistake
