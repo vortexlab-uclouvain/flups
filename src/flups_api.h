b3af2ae54255db902af83c150bf134664d5a44ca
tgillis <thomas.gillis@uclouvain.be>
2019-10-10 10:35:37 +0200

    :tada: first try for the API in C

---
+++ src/flups_api.h
@@ -0,0 +1,49 @@
+/**
+ * @file flups_api.h
+ * @author Thomas Gillis and Denis-Gabriel Caprace
+ * @brief 
+ * @version
+ * @date 2019-10-09
+ * 
+ * @copyright Copyright © UCLouvain 2019
+ * 
+ * FLUPS is a Fourier-based Library of Unbounded Poisson Solvers.
+ * 
+ * Copyright (C) <2019> <Université catholique de Louvain (UCLouvain), Belgique>
+ * 
+ * List of the contributors to the development of FLUPS, Description and complete License: see LICENSE file.
+ * 
+ * This program (FLUPS) is free software: 
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see COPYING file).  If not, 
+ * see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Solver FLUPS_solver;
+
+
+// get a new solve
+FLUPS_solver* flups_new_solver();
+// destroy the solver
+void flups_free_solver(FLUPS_solver*);
+// setup the solver
+void flups_setup(FLUPS_solver*);
+void flups_set_greenType(FLUPS_solver*);
+// solve
+void flups_solve(FLUPS_solver*);
+
+#ifdef __cplusplus
+}
+#endif
