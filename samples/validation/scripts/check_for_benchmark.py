0b7b976057c6fed42e2cac2523e349fcae13cd1d
DeeGee <denis-gabriel.caprace@uclouvain.be>
2020-05-19 20:17:37 +0200

    :white_check_mark: adapting scipt for checking results (equal to machine epsilon)

---
+++ samples/validation/scripts/check_for_benchmark.py
@@ -0,0 +1,43 @@
+import csv
+import sys
+
+from os import listdir
+from os.path import isfile, join
+
+#read the files located at path (argument of this script) and check if the error in the file is well equal to the machine epsilon 
+#return the number of mistakes
+
+#path 2 your results
+path = sys.argv[len(sys.argv)-1]
+
+#absolute tolerance to machine epsilon
+tol = 1e-12
+
+#Checking for exactness of results
+n_mistake = 0
+
+#list all files in path
+files = [f for f in listdir(path) if (isfile(join(path, f)) and '.txt' in f)]
+
+
+for file in files:
+    fcurr = open(path+'/'+file,'r')
+
+    print(path+'/'+file)
+
+    #comparing current results with reference
+    for line in csv.reader(fcurr,delimiter=' '):
+        buff = list(line)  
+        
+        err2   = abs(float(buff[1]))
+        errInf = abs(float(buff[2]))
+
+        if err2<tol and errInf<tol:
+            pass
+        else:
+            print("WRONG values in "+file+"  ("+buff[1]+" , " + buff[2] + " ... expected 0.0)" )
+            n_mistake +=1
+
+    fcurr.close()
+
+print("done, with %d error(s)."%n_mistake)
