d165b79ece417eadb977b0e06d048a0b40214837
Pierre Balty <pierre.balty@uclouvain.be>
2022-03-02 18:39:02 +0000

    Update test/run/run_test.sh

--- test/run/run_test.sh
+++ test/run/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Submission script for Lemaitre3 
 #SBATCH --job-name=flups_auto_test
-#SBATCH --time=1:30:00 # hh:mm:ss
+#SBATCH --time=2:30:00 # hh:mm:ss
 #
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=4000 # megabytes 
