9b3c32c31bb529c7ea9f00821e46024d2d5851ce
pbalty <pierre.balty@uclouvain.be>
2022-03-15 15:29:56 +0000

    :bug: Forgot to include the new file in the last commit

---
+++ src/toolsinterface.hpp
@@ -0,0 +1,24 @@
+#include "Solver.hpp"
+#include "FFTW_plan_dim_cell.hpp"
+#include "FFTW_plan_dim_node.hpp"
+
+
+int hint_proc_repartition(const int lda, const double h[3], const double L[3], FLUPS_BoundaryType* bc[3][2], const FLUPS_CenterType center_type[3]){
+    FFTW_plan_dim* plan[3];
+    for (int id = 0; id < 3; id++) {
+        if (CELL_CENTER == center_type[id]) {
+            plan[id]  = new FFTW_plan_dim_cell(lda, id, h, L, bc[id], FLUPS_FORWARD, false);
+        } else if (NODE_CENTER == center_type[id]) {
+            plan[id]  = new FFTW_plan_dim_node(lda, id, h, L, bc[id], FLUPS_FORWARD, false);
+        } else {
+            FLUPS_CHECK(false, "The type of data you asked is not supported", LOCATION);
+        }
+    }
+    
+    int ridx = sort_plans(plan); 
+
+    for (int id = 0; id < 3; id++) {
+        flups_free(plan[id]);
+    }
+    return ridx;
+}
