cb3d478f8271057b7c177f617d05de16b1531a09
pbalty <pierre.balty@uclouvain.be>
2022-07-13 17:10:03 +0200

    :zap: Trying new modules on mlx

--- samples/validation/run/benchmark_launch_workflow.sh
+++ samples/validation/run/benchmark_launch_workflow.sh
@@ -8,7 +8,7 @@
 # MPI_datatypes              = isr
 # First version all to all   = dprec_a2a 
 # First version non blocking = dprec_nb
-export CODE_VERSION='a2a'
+export CODE_VERSION='a2a isr nb'
 
 ## Kernels : 
 # CHAT_2 = 0, /**< @brief quadrature in zero, order 2, Chatelain et al. (2010) */
@@ -19,8 +19,8 @@ export CODE_VERSION='a2a'
 # HEJ_8  = 5, /**< @brief regularized, order 8, Hejlesen et al. (2015)*/
 # HEJ_10 = 6, /**< @brief regularized, order 10, Hejlesen et al. (2015)*/
 # HEJ_0  = 7, /**< @brief Fourier cutoff, spectral-like, Hejlesen et al. (2019)*/
-export CODE_KERNEL='0 1 2 3 4 5 6 7 8'
-#export CODE_KERNEL='0'
+#export CODE_KERNEL='0 1 2 3 4 5 6 7 8'
+export CODE_KERNEL='0'
 
 ## Center ; 
 # Node-centred = 0 
@@ -48,7 +48,7 @@ export COMPILE_TIME='00:10:00'
 export KERNEL_TIME='02:30:00'
 
 # ..................................................................
-export CLUSTER='vega'
+export CLUSTER='meluxina'
 
 # ..................................................................
 #---------------------------------------------------------------------------------------
@@ -134,8 +134,8 @@ fi
 
 
 # Performance 
-# ("${HOME_FLUPS}/samples/validation/run/benchmark_workflow_weak.sh")
+("${HOME_FLUPS}/samples/validation/run/benchmark_workflow_weak.sh")
 # ("${HOME_FLUPS}/samples/validation/run/benchmark_workflow_strong.sh")
 
 # Validation 
-("${HOME_FLUPS}/samples/validation/run/benchmark_workflow_validation.sh")
+#("${HOME_FLUPS}/samples/validation/run/benchmark_workflow_validation.sh")
--- samples/validation/run/meluxina_modules.sh
+++ samples/validation/run/meluxina_modules.sh
@@ -1,12 +1,18 @@
 
 ## Change the list of module we use 
-module use /apps/USE/easybuild/staging/2021.5/modules/all
+#module use /apps/USE/easybuild/staging/2021.5/modules/all
+#
+### First load the librairies relying on the old version of OpenMPI
+#module load HDF5/1.12.1-gompi-2021a
+#module load FFTW/3.3.10-gompi-2021a 
+#
+### Then load the latest version of OpenMPI
+##module load OpenMPI/5.0.0-GCC-10.3.0
+#module load OpenMPI/${1}-GCC-10.3.0
+##module load OpenMPI/4.1.1-GCC-10.3.0
 
-## First load the librairies relying on the old version of OpenMPI
+module use /apps/USE/easybuild/release/2021.5/modules/all
 module load HDF5/1.12.1-gompi-2021a
-module load FFTW/3.3.10-gompi-2021a 
-
-## Then load the latest version of OpenMPI
-#module load OpenMPI/5.0.0-GCC-10.3.0
-module load OpenMPI/${1}-GCC-10.3.0
-#module load OpenMPI/4.1.1-GCC-10.3.0
+module load FFTW/3.3.10-gompi-2021a
+module load OpenMPI/4.1.4-GCC-10.3.0
+module load CMake/3.20.1-GCCcore-10.3.0
