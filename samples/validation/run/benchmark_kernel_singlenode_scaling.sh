c7d9f0e94abeda80d0cc5db53f350be3023ecbb3
pbalty <pierre.balty@uclouvain.be>
2022-09-14 18:06:00 +0200

    :construction: working on a weak scaling test on a single node

---
+++ samples/validation/run/benchmark_kernel_singlenode_scaling.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -l
+#SBATCH --hint=nomultithread    # don't use hyperthreading
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+source ${SCRIPT_MODULE} ${MPI_VERSION}
+
+export NRES=1
+
+export ARR_NPROC_X=(1 2 2 2 4 4 4)
+export ARR_NPROC_Y=(1 1 2 2 2 4 4)
+export ARR_NPROC_Z=(1 1 1 2 2 2 4)
+
+for idx in "${!ARR_NPROC_X[@]}";
+do    
+    
+    export NPROC_X=${ARR_NPROC_X[$idx]}
+    export NPROC_Y=${ARR_NPROC_Y[$idx]}
+    export NPROC_Z=${ARR_NPROC_Z[$idx]}
+
+    export NGLOB_X=$(( ($NPROC_X)*($NPCPUS) ))
+    export NGLOB_Y=$(( ($NPROC_Y)*($NPCPUS) ))
+    export NGLOB_Z=$(( ($NPROC_Z)*($NPCPUS) ))
+
+    export L_X=$(( ($NPROC_X) ))
+    export L_Y=$(( ($NPROC_Y) ))
+    export L_Z=$(( ($NPROC_Z) ))
+
+    for version in ${CODE_VERSION}
+    do
+        for kernel in ${CODE_KERNEL}
+        do 
+            export EXEC_FLUPS=flups_validation_${version}
+            export SCRATCH_FLUPS=${SCRATCH_DIR}/simulations_${version}_N${NPROC_X}x${NPROC_Y}x${NPROC_Z}/
+            mkdir -p ${SCRATCH_FLUPS}/prof/
+
+            cd ${SCRATCH_FLUPS}
+            cp ${FLUPS_DIR}/samples/validation/${EXEC_FLUPS} ${SCRATCH_FLUPS}
+
+            echo "---------------- UCX Flags ---------------------"
+            echo "UCX_TLS == ${UCX_TLS}"
+            echo "OMPI_MCA_pml == ${OMPI_MCA_pml}"
+            echo "OMPI_MCA_osc == ${OMPI_MCA_osc}"
+
+            echo "----------------- launching job -----------------"
+            echo "Launching jobs with: "
+            echo "OMP_NUM_THREADS=1 ${LAUNCH_COMMAND} ./${EXEC_FLUPS} --np=${NPROC_X},${NPROC_Y},${NPROC_Z} --res=${NGLOB_X},${NGLOB_Y},${NGLOB_Z} --dom=${L_X},${L_Y},${L_Z} --bc=4,4,4,4,4,4 --nres=1 --nsolve=20 --kernel=${kernel} --center=${CODE_CENTER}"
+
+            OMP_NUM_THREADS=1 ${LAUNCH_COMMAND} ./${EXEC_FLUPS} --np=${NPROC_X},${NPROC_Y},${NPROC_Z} --res=${NGLOB_X},${NGLOB_Y},${NGLOB_Z} --dom=${L_X},${L_Y},${L_Z} --bc=4,4,4,4,4,4 --nres=${NRES} --nsolve=20 --kernel=${kernel} --center=${CODE_CENTER}
+            echo "----------------- done           -----------------"
+            cd -
+        done 
+    done
+done
