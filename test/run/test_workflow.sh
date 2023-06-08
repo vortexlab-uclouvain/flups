3d8736adff04bf1f7f1357e594b10a65d1ea1fa6
pbalty <pierre.balty@uclouvain.be>
2023-06-08 16:30:38 +0200

    :white_check_mark: Increase the number of jobs launched on lm3 to reduce the time needed for the test

--- test/run/test_workflow.sh
+++ test/run/test_workflow.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-KERNELS='CHAT2 LGF2 HEJ2 HEJ4 HEJ6 HEJ8 HEJ10 HEJ0'
-CENTERS='Node Cell'
+# KERNELS='CHAT2 LGF2 HEJ2 HEJ4 HEJ6 HEJ8 HEJ10 HEJ0'
+CENTERS='node cell'
 VERSIONS='nb a2a isr'
 
 export LIBDIR=$1
 
-for version in ${VERSIONS}
-do
-    export EXEC=flups_test_${version}
-    export REPORT=xml:report_test_${version}.xml
-    export FILE_OUT=std_out_${version}
-    export TESTS=AllTest/ConvergenceTest.AllBoundaryConditions
-    export LIBPATH=${LIBDIR}
-    echo "Submitting job with command:  sbatch --export=LIBPATH=${LIBDIR},REPORT=${REPORT},FILE_OUT=${FILE_OUT},TESTS=${TESTS},EXEC=${EXEC_FLUPS} test_convergence.sh" 
-    sbatch test_convergence.sh
+for version in ${VERSIONS}; do
+    for center in ${CENTERS}; do
+        export EXEC=flups_test_${version}
+        export REPORT=xml:report_test_${version}_${center}.xml
+        export FILE_OUT=std_out_${version}_${center}
+        export TESTS=AllTest/ConvergenceTest.AllBoundaryConditions/${center}
+        export LIBPATH=${LIBDIR}
+        echo "Submitting job with command: sbatch --export=LIBPATH=${LIBDIR},REPORT=${REPORT},FILE_OUT=${FILE_OUT},TESTS=${TESTS},EXEC=${EXEC_FLUPS} test_convergence.sh"
+        sbatch test_convergence.sh
+        echo "   "
+    done
 done
-
