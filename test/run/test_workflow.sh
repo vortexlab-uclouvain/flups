2c095c04a5da9166b60ed664203277318a3c7e89
Gilles Poncelet <gilles.poncelet@uclouvain.be>
2023-12-07 16:57:08 +0100

    :white_check_mark: further subdivide the tests to avoid running out of time

--- test/run/test_workflow.sh
+++ test/run/test_workflow.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-# KERNELS='CHAT2 LGF2 HEJ2 HEJ4 HEJ6 HEJ8 HEJ10 HEJ0'
 CENTERS='node cell'
 VERSIONS='nb a2a isr'
+KERNELS='chat2 lgf2 lgf4 lgf6 lgf8 hej2 hej4 hej6 hej8 hej10 hej0 mehr4l mehr6l mehr4f mehr4l'
 
 export LIBDIR=$1
 
 for version in ${VERSIONS}; do
     for center in ${CENTERS}; do
-        export EXEC=flups_test_${version}
-        export REPORT=xml:report_test_${version}_${center}.xml
-        export FILE_OUT=std_out_${version}_${center}
-        export TESTS=AllTest/ConvergenceTest.AllBoundaryConditions/${center}
-        export LIBPATH=${LIBDIR}
-        echo "Submitting job with command: sbatch --export=LIBPATH=${LIBDIR},REPORT=${REPORT},FILE_OUT=${FILE_OUT},TESTS=${TESTS},EXEC=${EXEC_FLUPS} test_convergence.sh"
-        sbatch test_convergence.sh
-        echo "   "
+        for kernel in ${KERNELS}; do
+            export EXEC=flups_test_${version}
+            export REPORT=xml:report_test_${version}_${center}_${kernel}.xml
+            export FILE_OUT=std_out_${version}_${center}_${kernel}
+            export TESTS=AllTest/ConvergenceTest.AllBoundaryConditions/${center}_${kernel}
+            export LIBPATH=${LIBDIR}
+            echo "Submitting job with command: sbatch --export=LIBPATH=${LIBDIR},REPORT=${REPORT},FILE_OUT=${FILE_OUT},TESTS=${TESTS},EXEC=${EXEC_FLUPS} test_convergence.sh"
+            sbatch test_convergence.sh
+            echo "   "
+        done
     done
 done
